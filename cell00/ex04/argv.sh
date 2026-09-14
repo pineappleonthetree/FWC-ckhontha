4ae2b8c311859846ef9d5bdd20c64351d6078d15
Pineapple <67070033@kmitl.ac.th>
2026-09-14 23:51:23 +0700

    Submission ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -3,11 +3,7 @@
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    for i in {1..3}; do
-        if [ $i -le $# ]; then
-            echo "${!i}"
-        else
-            break
-        fi
+    for i in $(seq 1 $#); do
+        echo "${!i}"
     done
 fi
