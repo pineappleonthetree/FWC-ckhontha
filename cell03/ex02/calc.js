c11b0b8de9053bc7362b728653ebdee26b318ecf
Pineapple <67070033@kmitl.ac.th>
2026-09-17 00:37:00 +0700

    Submission ex02

--- cell03/ex02/calc.js
+++ cell03/ex02/calc.js
@@ -4,52 +4,52 @@ document.addEventListener('DOMContentLoaded', function () {
 	var rightInput = document.getElementById('right');
 	var operatorSelect = document.getElementById('operator');
 
-	function isPositiveInteger(value) {
-		return /^\d+$/.test(value);
+function isPositiveInteger(value) {
+	return /^\d+$/.test(value);
+}
+
+function calculate(left, operator, right) {
+	switch (operator) {
+		case '+':
+			return left + right;
+		case '-':
+			return left - right;
+		case '*':
+			return left * right;
+		case '/':
+			return left / right;
+		case '%':
+			return left % right;
+		default:
+			return null;
+	}
+}
+
+form.addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	var leftValue = leftInput.value.trim();
+	var rightValue = rightInput.value.trim();
+	var operator = operatorSelect.value;
+
+	if (!isPositiveInteger(leftValue) || !isPositiveInteger(rightValue)) {
+		alert('Error :(');
+		return;
 	}
 
-	function calculate(left, operator, right) {
-		switch (operator) {
-			case '+':
-				return left + right;
-			case '-':
-				return left - right;
-			case '*':
-				return left * right;
-			case '/':
-				return left / right;
-			case '%':
-				return left % right;
-			default:
-				return null;
-		}
+	var left = parseInt(leftValue, 10);
+	var right = parseInt(rightValue, 10);
+
+	if ((operator === '/' || operator === '%') && right === 0) {
+		alert("It's over 9000!");
+	    return;
 	}
 
-	form.addEventListener('submit', function (event) {
-		event.preventDefault();
+	var result = calculate(left, operator, right);
 
-		var leftValue = leftInput.value.trim();
-		var rightValue = rightInput.value.trim();
-		var operator = operatorSelect.value;
-
-		if (!isPositiveInteger(leftValue) || !isPositiveInteger(rightValue)) {
-			alert('Error :(');
-			return;
-		}
-
-		var left = parseInt(leftValue, 10);
-		var right = parseInt(rightValue, 10);
-
-		if ((operator === '/' || operator === '%') && right === 0) {
-			alert("It's over 9000!");
-			return;
-		}
-
-		var result = calculate(left, operator, right);
-
-		console.log(result);
-		alert(result);
-	});
+	console.log(result);
+	alert(result);
+});
 
 	setInterval(function () {
 		alert('Please, use me...');
